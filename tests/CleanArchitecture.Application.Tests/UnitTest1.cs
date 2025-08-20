706af1990d003affe4e4e99cd4afec8823235eca
Viet Bui <ldcc.vietbui@lotte.vn>
2025-08-20 11:38:25 +0700

    Add Unit test project.

---
+++ tests/CleanArchitecture.Application.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchitecture.Application.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/CleanArchitecture.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchitecture.Infrastructure.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
